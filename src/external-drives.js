7b89ceab1d84eb225c312d31b12d11e422679161
Rick Medina <rickmedina85@gmai.com>
2017-01-20 00:21:56 -0400

    Added partitions info to external-devices module label, mountpoint, name, size

--- src/external-drives.js
+++ src/external-drives.js
@@ -8,23 +8,30 @@ const execFileCB = cb => (err, stdout, stderr) => {
   else {
     const devInfo = JSON.parse(stdout)['blockdevices']
       .filter(x => x.hotplug === '1')
-      .map(x => ({
-        vendor: x.vendor,
-        model: x.model,
-        size: x.size,
-        sysName: x.name
-      }))
+      .map(x => {
+        const partitions = x.children
+          .map(x => {
+            const {name, size, label, mountpoint} = x
+            return {name, size, label, mountpoint}
+          })
+        const {vendor, model, size, name} = x
+        return { vendor, model, size, name, partitions }
+      })
     cb(null, devInfo)
   }
 }
 
 // cb(err, data) -> child_process
-// data: [{vendor: str, model:str, size:str, sysName: str}]
+// data: [{vendor: str, model:str, size:str, name: str}]
 const externalDrivesImp = (execFile, isRoot) => cb => {
   if (!isRoot()) cb(new Error('You must run as root'))
   else return execFile(
     `lsblk`,
-    ['--nodeps', '--output=NAME,SIZE,VENDOR,MODEL,HOTPLUG', '-J'],
+    [
+      '--nodeps',
+      '--output=NAME,SIZE,VENDOR,MODEL,HOTPLUG,LABEL,MOUNTPOINT',
+      '--json'
+    ],
     {maxBuffer: 200*1024*1024},
     execFileCB(cb)
   )
--- src/external-drives.test.js
+++ src/external-drives.test.js
@@ -1,4 +1,7 @@
-const {externalDrivesImp, execFileCB} = require('./external-drives')
+const {
+  externalDrivesImp,
+  execFileCB
+} = require('./external-drives')
 
 describe('external-drives', () => {
 
@@ -11,34 +14,80 @@ describe('external-drives', () => {
           "size": "232.9G",
           "vendor": "ATA     ",
           "model": "Samsung SSD 840 ",
-          "hotplug": "0"
+          "hotplug": "0",
+          "label": null,
+          "mountpoint": null,
+          "children": [{
+            "name": "sda10",
+            "size": "50.1G",
+            "vendor": null,
+            "model": null,
+            "hotplug": "0",
+            "label": null,
+            "mountpoint": null
+          }]
         }, {
           "name": "sdb",
           "size": "3.8G",
           "vendor": "Generic ",
           "model": "Flash Disk      ",
-          "hotplug": "1"
+          "hotplug": "1",
+          "label": null,
+          "mountpoint": null,
+          "children": [{
+            "name": "sdb1",
+            "size": "3.8G",
+            "vendor": null,
+            "model": null,
+            "hotplug": "1",
+            "label": "LINUX MINT",
+            "mountpoint": null
+          }]
         }, {
           "name": "sdc",
           "size": "465.8G",
           "vendor": "ST500LM0",
           "model": "00-SSHD-8GB     ",
-          "hotplug": "1"
+          "hotplug": "1",
+          "label": null,
+          "mountpoint": null,
+          "children": [{
+            "name": "sdc5",
+            "size": "5.9G",
+            "vendor": null,
+            "model": null,
+            "hotplug": "1",
+            "label": "WIN10_MEDIA",
+            "mountpoint": null
+          }]
         }]
       }`
+
       const _cb = jest.fn()
       execFileCB(_cb)(null, stdout, null)
 
       const exp = [{
-        sysName: 'sdb',
-        size: '3.8G',
-        vendor: 'Generic ',
-        model: 'Flash Disk      '
+        "model": "Flash Disk      ",
+        "name": "sdb",
+        "partitions": [{
+          "label": "LINUX MINT",
+          "mountpoint": null,
+          "name": "sdb1",
+          "size": "3.8G"
+        }],
+        "size": "3.8G",
+        "vendor": "Generic "
       }, {
-        sysName: 'sdc',
-        size: '465.8G',
-        vendor: 'ST500LM0',
-        model: '00-SSHD-8GB     '
+        "model": "00-SSHD-8GB     ",
+        "name": "sdc",
+        "partitions": [{
+          "label": "WIN10_MEDIA",
+          "mountpoint": null,
+          "name": "sdc5",
+          "size": "5.9G"
+        }],
+        "size": "465.8G",
+        "vendor": "ST500LM0"
       }]
 
       expect(_cb).toBeCalledWith(null, exp)
@@ -66,11 +115,17 @@ describe('external-drives', () => {
       externalDrivesImp(_execFile, _isRoot)(() => '')
       const expFirs3Args = [
         'lsblk',
-        ['--nodeps', '--output=NAME,SIZE,VENDOR,MODEL,HOTPLUG', '-J'],
-        {maxBuffer: 200*1024*1024}
+        [
+          '--nodeps',
+          '--output=NAME,SIZE,VENDOR,MODEL,HOTPLUG,LABEL,MOUNTPOINT',
+          '--json'
+        ],
+        {
+          maxBuffer: 200 * 1024 * 1024
+        }
       ]
       const expLastArg = execFileCB(() => '').toString()
-      const actFirs3Args = _execFile.mock.calls[0].slice(0,3)
+      const actFirs3Args = _execFile.mock.calls[0].slice(0, 3)
       const actLastArg = _execFile.mock.calls[0][3].toString()
       expect(actFirs3Args).toEqual(expFirs3Args)
       expect(actLastArg).toBe(expLastArg)
