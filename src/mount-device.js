6b2810fcae192599850731ab31dfb1e62d32e8a4
Rick Medina <rickmedina85@gmai.com>
2017-01-20 13:44:53 -0400

    finished mount-device module

--- src/mount-device.js
+++ src/mount-device.js
@@ -6,7 +6,7 @@ const {mkdir, rmdir} = require('fs')
 const execHandlerImp = rmdir => (mountPath, cb) => (err, stdout, stderr) => {
   if (err) {
     rmdir(mountPath, err => {
-      err.mountDevice = 'Remove mountPath while cleanup failed'
+      err.mountDevice = 'Remove mountPath failed on cleanup'
       cb(err)
     })
   }
@@ -15,14 +15,18 @@ const execHandlerImp = rmdir => (mountPath, cb) => (err, stdout, stderr) => {
 
 const execHandler = execHandlerImp(rmdir)
 
+
 // str -> (err, data) -> ()
 // data is the mounted path
 const mountDeviceImp = (execFile, mkdir) => (devName, cb) => {
   const reTryIfDirExist = num => {
     const mountPath = `/tmp/${devName}-${num}`
     mkdir(mountPath, err => {
-      if (num === 5) cb(err)
-      else if (err.code === 'EEXIST') {
+      if (num === 5) {
+        err.mountDevice = `Failed after trying to create mounting dirs with ${num} different names`
+        cb(err)
+      }
+      else if (err && err.code === 'EEXIST') {
         reTryIfDirExist(num + 1)
       }
       else if (err) cb(err)
--- src/mount-device.test.js
+++ src/mount-device.test.js
@@ -1,69 +1,62 @@
-const { mountDeviceImp, execHandlerImp } = require('./external-drives')
+const {mountDeviceImp, execHandlerImp} = require('./mount-device')
 
 describe('external-drives', () => {
 
+  let _cb
+  beforeEach( () => _cb = jest.fn() )
+
   describe('execHandlerImp', () => {
 
-    const somePath = '/dsad/dasd'
-    const someErr = 'someErr'
+    const SOME_PATH = '/dsad/dasd'
+    const SOME_ERR = new Error
+    let _rmdir
+    beforeEach( () => _rmdir = jest.fn() )
+
+    it('should call rmdir and the cb with correct params on execFile err', () => {
+      _rmdir = jest.fn( (str, cb) => cb(SOME_ERR) )
+      execHandlerImp(_rmdir)(SOME_PATH, _cb)(SOME_ERR, null, null)
+      expect(_cb.mock.calls[0][0].mountDevice).toBeDefined()
+    });
 
     it('should call the cb with correct data if no err or stderr', () => {
-      const _cb = jest.fn()
-      execHandlerImp(somePath, _cb)(null, null, null)
-      expect(_cb).toBeCalledWith(null, somePath)
+      execHandlerImp(_rmdir)(SOME_PATH, _cb)(null, null, null)
+      expect(_cb).toBeCalledWith(null, SOME_PATH)
+      expect(_rmdir).not.toBeCalled()
     });
-
-    it('should call the cb with err on execFile err', () => {
-      const _cb = jest.fn()
-      execHandlerImp(somePath, _cb)(someErr, null, null)
-      expect(_cb).toBeCalledWith(someErr)
-    });
-
   });
 
   describe('mountDevice', () => {
 
-    let _cb, _execFile, _mkdir
-    beforeEach(() => {
-      _cb = jest.fn()
-      _execFile = jest.fn()
-      _mkdir = jest.fn()
-    })
+    const DEV_NAME = `sdc2`
+    let _execFile
+    beforeEach( () => _execFile = jest.fn() )
 
-    it('should call execFile with the correct args on success', () => {
-
-
-
-      mountDeviceImp(_execFile, _mkdir)('sdb1')
-
-
-
-      const _execFile = jest.fn()
-      const _isRoot = jest.fn(() => true)
-      const expFirs3Args = [
-        'lsblk',
-        [
-          '--nodeps',
-          '--output=NAME,SIZE,VENDOR,MODEL,HOTPLUG,LABEL,MOUNTPOINT',
-          '--json'
-        ],
-        {
-          maxBuffer: 200 * 1024 * 1024
-        }
-      ]
-      const expLastArg = execHandlerImp(() => '').toString()
-      const actFirs3Args = _execFile.mock.calls[0].slice(0, 3)
-      const actLastArg = _execFile.mock.calls[0][3].toString()
-      expect(actFirs3Args).toEqual(expFirs3Args)
-      expect(actLastArg).toBe(expLastArg)
+    it('should call cb with correct err msg on mkdir fail', () => {
+      const _mkdir = jest.fn( (str, cb) => cb({code: 'EEXIST'}) )
+      mountDeviceImp(_execFile, _mkdir)(DEV_NAME, _cb)
+      expect(_cb.mock.calls[0][0].mountDevice).toBeDefined()
     });
 
-    it('should call cb with err if process was not ran as root', () => {
-      const _cb = jest.fn()
-      const _execFile = jest.fn()
-      const _isRoot = jest.fn(() => false)
-      mountDeviceImp(_execFile, _isRoot)(_cb)
+    it('should retry mkdir on mkdir fail', () => {
+      const _mkdir = jest.fn( (str, cb) => cb({code: 'EEXIST'}) )
+      mountDeviceImp(_execFile, _mkdir)(DEV_NAME, _cb)
+      expect(_mkdir.mock.calls.length).toBeGreaterThan(2)
+    });
+
+    it('should call cb with mkdir err on mkdir fail', () => {
+      const _mkdir = jest.fn( (str, cb) => cb(new Error) )
+      mountDeviceImp(_execFile, _mkdir)(DEV_NAME, _cb)
       expect(_cb.mock.calls[0][0]).toBeInstanceOf(Error)
+    });
+
+    it('should call execFile with correct params on mkdir success', () => {
+      const _mkdir = jest.fn( (str, cb) => cb(null) )
+      mountDeviceImp(_execFile, _mkdir)(DEV_NAME, _cb)
+      const firstCall = _execFile.mock.calls[0]
+      expect(firstCall[0]).toBe('mount')
+      expect(firstCall[1][0]).toBe(`/dev/${DEV_NAME}`)
+      expect(firstCall[1][1]).toMatch(new RegExp(`/tmp/${DEV_NAME}-\\d`))
+      expect(firstCall[2].length).toBe(3)
     });
 
   });
