4f10a103d29603029817b9247ea29e148b8b0c48
Rick Medina <rickmedina85@gmai.com>
2017-01-21 14:16:10 -0400

    added comment regarding why no DI

--- src/is-root.js
+++ src/is-root.js
@@ -1,3 +1,5 @@
+// this function should have DI with process on param but is not to
+// learn how to mock global objects in test
 exports.isRoot = () => {
   if (!process.getuid) return new Error('You are not in a POSIX system')
   else return process.getuid() === 0
