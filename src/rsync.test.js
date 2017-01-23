349616ded43e4272cec942bb325b035634b6cd95
Rick Medina <rickmedina85@gmai.com>
2017-01-23 15:02:44 -0400

    rsync now returns child process and does not accept callback spawn (best for long running process) can be composed by caller as stream

--- src/rsync.test.js
+++ src/rsync.test.js
@@ -1,44 +1,22 @@
 const {__rsync} = require('./rsync')
-const EventEmitter = require('events')
 
-class Emitter extends EventEmitter {}
 const PATH = '/some/path'
 const PATTERNS = ["/proc/*", "/sys/*"]
 
-let _spawn, _cb, _child
-
-beforeEach(() => {
-  _spawn = jest.fn( () => {
-    _child = new Emitter()
-    _child.stderr = new Emitter()
-    return _child
-  })
-  _cb = jest.fn()
-})
-
 it('calls spawn with the correct params', () => {
-  __rsync(_spawn)(PATH, PATH, PATTERNS, _cb)
+  const _spawn = jest.fn()
+  __rsync(_spawn)(PATH, PATH, PATTERNS)
   expect(_spawn.mock.calls).toMatchSnapshot()
 });
 
 it('calls spawn with the correct params of no patterns are passed', () => {
-  __rsync(_spawn)(PATH, PATH, undefined, _cb)
+  const _spawn = jest.fn()
+  __rsync(_spawn)(PATH, PATH, undefined)
   expect(_spawn.mock.calls).toMatchSnapshot()
 });
 
-it('should return a child_process', () => {
-  const res = __rsync(_spawn)(PATH, PATH, PATTERNS, _cb)
-  expect(res.on).toBeDefined()
-});
-
-it('spawn on error event works ok', () => {
-  __rsync(_spawn)(PATH, PATH, PATTERNS, _cb)
-  _child.emit('error', 'some err')
-  expect(_cb).toBeCalledWith('some err')
-});
-
-it('spawn stderr on error event works ok', () => {
-  __rsync(_spawn)(PATH, PATH, PATTERNS, _cb)
-  _child.stderr.emit('data', 'some data')
-  expect(_cb).toBeCalledWith('some data')
+it('should return the child_process', () => {
+  const _spawn = jest.fn( () => 'test')
+  const res = __rsync(_spawn)(PATH, PATH, PATTERNS)
+  expect(res).toBe('test')
 });
